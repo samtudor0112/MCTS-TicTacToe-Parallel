e1df97324f40096358e65569c1987b7774d53854
Sam Tudor <samueltudor@hotmail.com>
2020-09-04 17:51:53 +1000

    added node and main files

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 4/9/20.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,18 @@
+//
+// Created by sam on 4/9/20.
+//
+
+#ifndef SERIAL_NODE_H
+#define SERIAL_NODE_H
+
+
+class Node {
+private:
+    Node parentNode;
+
+public:
+
+};
+
+
+#endif //SERIAL_NODE_H
