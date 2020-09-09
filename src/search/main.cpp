89d7ec057ba2c486e11277d38db47870740c6dc1
Sam Tudor <samueltudor@hotmail.com>
2020-09-09 15:02:00 +1000

    Small change to main

--- src/search/main.cpp
+++ src/search/main.cpp
@@ -3,20 +3,28 @@
 #include "../../include/gameboard/State.hpp"
 #include "../../include/search/MCTS.hpp"
 
-// The main entry point of the program. Currently plays a game of Tic Tac Toe against itself, with each player having
-// 3 seconds to play.
-int main(int argc, char** argv) {
-    State currentState = State();
-
+// Plays a game of Tic Tac Toe against itself, with each playing having timeToUse seconds to play each move
+GameStatus play_game(State startState, double timeToUse, bool print) {
+    State currentState = startState;
     while(true) {
-        double timeToUse = 3;
         MCTS search = MCTS(currentState, timeToUse);
         State bestMove = search.getBestMove();
-        std::cout << bestMove.getStringBoard() << std::flush;
+        if (print) {
+            std::cout << bestMove.getStringBoard() << std::flush;
+        }
         if (bestMove.getGameStatus() != inProgress) {
-            break;
+            return bestMove.getGameStatus();
         }
         currentState = bestMove;
     }
-    std::cout << "Done!" <<std::flush;
 }
+
+// The main entry point of the program. Currently plays a game with 3 second time limit and prints the board
+int main(int argc, char** argv) {
+    std::cout << "Start\n" << std::flush;
+    State currentState = State();
+    play_game(currentState, 3, true);
+    std::cout << "Done!\n" << std::flush;
+}
+
+
