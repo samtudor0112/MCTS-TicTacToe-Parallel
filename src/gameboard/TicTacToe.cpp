d51a5cb7609b2eeb533ee658d54ace9bc77c5e10
Sam Tudor <samueltudor@hotmail.com>
2020-11-04 15:45:12 +1000

    Rules should now work for d=3 (but only d=3)

--- src/gameboard/TicTacToe.cpp
+++ src/gameboard/TicTacToe.cpp
@@ -30,17 +30,18 @@ std::vector<TicTacToe> TicTacToe::getAllLegalMoveStates(PlayerColour turn) {
     std::vector<TicTacToe> out = std::vector<TicTacToe>();
     int[] newBoard;
     int[] oldBoard = turn == white ? whiteBoard : blackBoard;
+    int[] otherBoard = turn == white ? blackBoard : whiteBoard;
 
     for (int i = 0; i < size; i++) {
-        if (oldBoard[i] == 0) {
-            newBoard = malloc(sizeof(int)) * size;
-            memcpy(newBord, oldBoard, sizeof(int) * size);
+        if (oldBoard[i] == 0 && otherBoard[i] == 0) {
+            newBoard = malloc(sizeof(int) * size);
+            memcpy(newBoard, oldBoard, sizeof(int) * size);
             newBoard[i] = 1;
             TicTacToe newGame;
             if (turn == white) {
-                newGame = TicTacToe(n, d, newBoard, blackBoard, numMoves + 1);
+                newGame = TicTacToe(n, d, newBoard, otherBoard, numMoves + 1);
             } else {
-                newGame =  TicTacToe(n, d, whiteBoard, newBoard, numMoves + 1);
+                newGame =  TicTacToe(n, d, otherBoard, newBoard, numMoves + 1);
             }
             newGame.updateGameStatus(turn, i);
             out.push_back(newGame);
@@ -57,12 +58,230 @@ void TicTacToe::updateGameStatus(PlayerColour lastTurn, int lastPosition) {
     gameStatus win = lastTurn == white ? whiteWin : blackWin;
 
     // Check every row, column, diagonal to see if someone has won
-    int sum;
-    // TODO
-    for (int dimsChanged = 1; dimsChanged <= d; dimsChanged++) {
-        // TODO
+    // This only works for d=3. Solving the generalized problem for any d is nontrivially difficult, and is not the point of this project
+    // Being able to vary to any n gives us sufficient scalability of the problem to test our program.
+    int lastX = lastPosition % n;
+    int lastY = (lastPosition / n) % n;
+    int lastZ = lastPosition / (n * n);
+
+
+    // Single direction lines
+    int completed_line = 1;
+
+    for (int i = 0; i < n; i++) {
+        // X line
+        if(board[i + lastY * n + lastZ * n * n] == 0) {
+            // We could use a goto here but... yea
+            completed_line = 0;
+            break;
+        }
+    }
+    if (completed_line) {
         this->status = win;
         return;
+    }
+
+    completed_line = 1;
+
+    for (int i = 0; i < n; i++) {
+        // Y line
+        if(board[lastX + i * n + lastZ * n * n] == 0) {
+            // We could use a goto here but... yea
+            completed_line = 0;
+            break;
+        }
+    }
+    if (completed_line) {
+        this->status = win;
+        return;
+    }
+
+
+    int completed_line = 1;
+
+    for (int i = 0; i < n; i++) {
+        // Z line
+        if(board[lastX + lastY * n + i * n * n] == 0) {
+            // We could use a goto here but... yea
+            completed_line = 0;
+            break;
+        }
+    }
+    if (completed_line) {
+        this->status = win;
+        return;
+    }
+
+    // 2 direction lines
+    if (lastX == lastY) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // X-Y diagonal
+            if(board[i + i * n + lastZ * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastX == lastZ) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // X-Z diagonal
+            if(board[i + lastY * n + i * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastY == lastZ) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Y-Z diagonal
+            if(board[lastX + i * n + i * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastX == n - lastY - 1) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Other X-Y diagonal
+            if(board[i + (n - i - 1) * n + lastZ * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return
+        }
+    } 
+
+    if (lastX == n - lastZ - 1) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Other X-Z diagonal
+            if(board[i + lastY * n + (n - i - 1) * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    } 
+
+    if (lastY == n - lastZ - 1) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Other Y-Z diagonal
+            if(board[lastX + i * n + (n - i - 1) * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    // 3 direction lines
+    if (lastX == lastY && lastX == lastZ) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // X-Y-Z diagonal
+            if(board[i + i * n + i * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastX == lastY && lastX == n - lastZ - 1) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Next X-Y-Z diagonal
+            if(board[i + i * n + (n - i - 1) * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastX == n - lastY - 1 && lastX == lastZ) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Another X-Y-Z diagonal
+            if(board[i + (n - i - 1) * n + i * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
+    }
+
+    if (lastX == n - lastY - 1 && lastX == n - lastZ - 1) {
+        completed_line = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Last X-Y-Z diagonal
+            if(board[i + (n - i - 1) * n + (n - i - 1) * n * n] == 0) {
+                // We could use a goto here but... yea
+                completed_line = 0;
+                break;
+            }
+        }
+        if (completed_line) {
+            this->status = win;
+            return;
+        }
     }
 
     // Check if it's drawn or still going
